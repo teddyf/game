3918d6c04cc49f2d353109a8c45dcd96806d1e24
Teddy Franceschi <ttf6@duke.edu>
2016-09-11 22:07:53 -0400

    Removed extraneous imports

--- src/HealthBar.java
+++ src/HealthBar.java
@@ -1,8 +1,3 @@
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
